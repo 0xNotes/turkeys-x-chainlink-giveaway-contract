e1ec2cd13f07a8ff1d4bea2d6c56241ae2f78304
0xNotes <notes@sigil.systems>
2022-08-05 15:42:02 -0500

    Added VRF Basics

--- src/Giveaway.sol
+++ src/Giveaway.sol
@@ -3,9 +3,51 @@ pragma solidity ^0.8.13;
 
 
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+contract Giveaway is VRFConsumerBaseV2{
+
+    VRFCoordinatorV2Interface COORDINATOR;
+
+  // Your subscription ID.
+  uint64 s_subscriptionId;
 
 
-contract Giveaway{
+    //!! CHANGE TO KOVAN
+
+  // Rinkeby coordinator. For other networks,
+  // see https://docs.chain.link/docs/vrf-contracts/#configurations
+  address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+
+
+  // The gas lane to use, which specifies the maximum gas price to bump to.
+  // For a list of available gas lanes on each network,
+  // see https://docs.chain.link/docs/vrf-contracts/#configurations
+  bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+
+
+
+  // Depends on the number of requested values that you want sent to the
+  // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+  // so 100,000 is a safe default for this example contract. Test and adjust
+  // this limit based on the network that you select, the size of the request,
+  // and the processing of the callback request in the fulfillRandomWords()
+  // function.
+  uint32 callbackGasLimit = 100000;
+
+  // The default is 3, but you can set this higher.
+  uint16 requestConfirmations = 3;
+
+  // For this example, retrieve 2 random values in one request.
+  // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+  uint32 numWords =  2;
+
+  uint256[] public s_randomWords;
+  uint256 public s_requestId;
+  address s_owner;
+
+
 
     address targetNFT;
     //Used to store old winners in case they dont respond to winning.
@@ -15,9 +57,15 @@ contract Giveaway{
     event ChooseWinner(address winner, uint256 id);
 
 
-    constructor() {
-        owner = msg.sender;
-    }
+  constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+    COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+    s_owner = msg.sender;
+    s_subscriptionId = subscriptionId;
+    owner = msg.sender;
+
+  }
+
+
 
 
     modifier onlyOwner() {
@@ -33,9 +81,28 @@ contract Giveaway{
     }
 
 
+  // Assumes the subscription is funded sufficiently.
+  function requestRandomWords() external onlyOwner {
+    // Will revert if subscription is not set and funded.
+    s_requestId = COORDINATOR.requestRandomWords(
+      keyHash,
+      s_subscriptionId,
+      requestConfirmations,
+      callbackGasLimit,
+      numWords
+    );
+  }
 
-    function runGiveaway() public onlyOwner returns(uint256) {
+  function fulfillRandomWords(
+    uint256, /* requestId */
+    uint256[] memory randomWords
+  ) internal override {
+    s_randomWords = randomWords;
+  }
 
+
+    function runGiveaway(uint256 _idMax) public onlyOwner returns(uint256) {
+        
 
     } 
 
