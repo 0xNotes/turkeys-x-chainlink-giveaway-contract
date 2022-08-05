77e04bcd0092379a7b2f4e23cc9c655ae1765eab
0xNotes <notes@sigil.systems>
2022-08-05 14:44:37 -0500

    FakeTurkeys are created, ERC721 imported into giveaway contract

---
+++ src/Giveaway.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BNSL 1.1
+pragma solidity ^0.8.13;
+
+
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+
+contract Giveaway{
+
+    
+}
---
+++ src/TestMinter.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BNSL 1.1
+pragma solidity ^0.8.13;
+
+//this just needs to supply NFTs to some of the contract addresse
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract TestMinter is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("FakeTurkey", "FTY") {}
+
+    function mintNFT(address recipient) public returns (uint256) {
+        _tokenIdCounter.increment();
+        uint256 newItemId = _tokenIdCounter.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, "testUri");
+        return newItemId;
+    }
+
+    function multiMint(address[] memory _recipients, uint256 numberOfTurns)
+        public
+    {
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            for (uint256 j = 0; j < numberOfTurns; j++) {
+                mintNFT(_recipients[i]);
+            }
+        }
+    }
+}
+
