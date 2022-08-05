e6228428f8d45a9ea134c5ef100ed0696970dac4
0xNotes <notes@sigil.systems>
2022-08-05 14:59:14 -0500

    added basic security features

--- src/Giveaway.sol
+++ src/Giveaway.sol
@@ -7,5 +7,40 @@ import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract Giveaway{
 
-    
+    //Address of Turkeys to iterate over
+    address targetNFT;
+    //Used to store old winners in case they dont respond to winning.
+    address[] previousWinners;
+    address owner;
+    bool internal locked;
+    event ChooseWinner(address winner, uint256 id);
+
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+
+
+    function runGiveaway() public returns(uint256) {
+
+
+
+    } 
+
+
+
 }
